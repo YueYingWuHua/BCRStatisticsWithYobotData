7db3edcc87462e17f6457c1ab0f24ea7bdf8677c
gaoze <gaoze@cjbdi.com>
2020-06-09 16:13:30 +0800

    大幅修改了代码结构

--- WebContent/js/readStatisticsJson.js
+++ WebContent/js/readStatisticsJson.js
@@ -1,7 +1,7 @@
    let header = document.querySelector('header');
    let section = document.querySelector('section');
-   var file = 'json/20200607.json'
-   
+   var file = 'json/20200607.json'   
+	   
    function getJsonData() {
 	   let requestURL = 'json/20200607.json';
 	   let request = new XMLHttpRequest();
@@ -14,8 +14,7 @@
 	       let json = JSON.parse(text);
 	       console.log(json)
 	       	       
-	   };
-	   
+	   };	   
    }
  
    function getQQByName(jsonObj, name) {
@@ -34,8 +33,102 @@
 			   return members[i].nickname			  
 	   }
 	   return 0
-   }   
+   }
+   
+   /**
+    * 根据原始数据值获取每个用户的每个boss总伤害。
+    * @param jsonObj
+    * @returns Map[QQ号，伤害数组]
+    */   
+   function getAllUserDamage(jsonObj) {
+	   let challenges = jsonObj['challenges'];
+	   //包含30个qq对应的全部伤害数组
+	   //0-4 龙鸟猪鹿牛，5-9二+周目龙鸟猪鹿牛
+	   var dmgMap = new Map();
+	   for (i = 0; i < challenges.length; i++) {
+           let qqid = challenges[i].qqid;
+           let cycle = 0;
+           if (challenges[i].cycle > 1) cycle = 1;
+           if (dmgMap.get(qqid) != null){        	   
+        	   dmgMap.get(qqid)[cycle * 5 + challenges[i].boss_num - 1] += challenges[i].damage
+           } else {
+        	   var arr = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+        	   arr[cycle * 5 + challenges[i].boss_num - 1] += challenges[i].damage
+        	   dmgMap.set(qqid, arr)
+           }
+	   }
+	   return dmgMap
+   }
+   
+   function getAllDamagedPlayer(dmgMap, jsonObj) {	   
+	   var dmgPlayer = new Array()
+	   for (let [qqid, dmgArr] of dmgMap){
+		   let name = getNameByQQ(jsonObj, qqid)
+		   dmgPlayer.push({ "name": name, "qqid": qqid})
+	   }
+	   return dmgPlayer
+   }
  
+   /**
+    * 根据每个用户的总伤害，以及权重，算出每个用户的权重分值    
+    * @param dmgMap
+    * @param valueMap
+    * @returns Map[qqid, 权重分数组]
+    */
+   function getWeightedDmg(dmgMap, valueMap) {
+	   var weightedDmgMap = new Map();
+   	   for (let [qqid, dmgArr] of dmgMap) {
+   	   	   let qqWeightedDmgArr = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+           for(i = 0; i < 10; i++){
+			   qqWeightedDmgArr[i] = dmgArr[i] * valueMap[i] 
+		   }
+		   weightedDmgMap.set(qqid, qqWeightedDmgArr)
+       }
+	   return weightedDmgMap
+   }
+   
+   /**
+    * 根据每个boss的伤害算出来每个boss刀均伤害
+    * @param jsonObj
+    * @returns 伤害数组
+    */
+   function getAvgDmg(jsonObj) {
+	   let challenges = jsonObj['challenges'];
+	   var totalDmg = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+	   var count = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+	   for (i = 0; i < challenges.length; i++) {
+		   let cycle = 0;
+           if (challenges[i].cycle > 1) cycle = 1;
+           //不超过20W的刀没必要统计，不符合统计规律
+           if (challenges[i].damage > 200000){
+        	   totalDmg[cycle * 5 + challenges[i].boss_num - 1] += challenges[i].damage
+        	   count[cycle * 5 + challenges[i].boss_num - 1] += 1
+           }
+	   }
+	   var avgDmg = new Array(10)
+	   for (i = 0; i < 10; i++){
+		   if (count[i] > 0)
+			   avgDmg[i] = totalDmg[i]/count[i]
+	   } 
+	   return avgDmg 
+   }
+   
+   /**
+    * 根据平均伤害算出不同Boss的权值系数
+    * ∑avg
+    * @param avgDmg
+    * @returns 权值数组
+    */
+   function generateValue(avgDmg) {
+	   let totalDmg = 0
+	   avgDmg.map(dmg => {
+		   totalDmg += dmg
+	   })
+	   return avgDmg.map(dmg => {
+		   return 1/(dmg*10/totalDmg)
+	   })
+   }
+      
    function statisticPlayerByBoss(jsonObj, qq) {	   
 	   let challenges = jsonObj['challenges'];	   
 	   console.log('进入分析流程')
@@ -63,7 +156,7 @@
 
 	   }
 	   let myArticle = document.createElement('article');
-	   let myH4 = document.createElement('h4');
+	   let myH4 = document.createElement('h1');
 	   myH4.textContent = getNameByQQ(jsonObj, qq)+'的离职证明';
 	   myArticle.appendChild(myH4);
 	   header.appendChild(myArticle);
